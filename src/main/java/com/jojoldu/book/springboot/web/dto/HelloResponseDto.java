950f9281b464beb4238b4c48a7faf2fb6cc1032b
Yunhye-Choi <anffl98@gmail.com>
2021-01-17 12:48:07 +0900

    2-3 까지 완료

--- src/main/java/com/jojoldu/book/springboot/web/dto/HelloResponseDto.java
+++ src/main/java/com/jojoldu/book/springboot/web/dto/HelloResponseDto.java
@@ -7,5 +7,5 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor // 모든 final 필드가 포함된 생성자를 생성
 public class HelloResponseDto {
     private final String name;
-    private final int amount;
+    private final int amount; //
 }
